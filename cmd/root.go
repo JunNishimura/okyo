ad892f1c7973166aeb334c4c439d5268761fc2aa
Jun Nishimura <n.junjun0303@gmail.com>
2024-04-27 17:55:55 +0900

    update command

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,29 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
 
+	"github.com/JunNishimura/okyo/internal/speaker"
 	"github.com/spf13/cobra"
 )
 
-
+var (
+	loopCount int
+	playSpeed float64
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "okyo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A cli tool to chant a hannya shingyo",
+	Long: "A cli tool to chant a hannya shingyo",
+	Run: func(cmd *cobra.Command, args []string) {
+		speaker := speaker.NewMp3Speaker(loopCount, playSpeed)
+		speaker.Play()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,15 +36,7 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.okyo.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().IntVarP(&loopCount, "count", "c", 1, "The number of times to play the audio(-1 means infinite loop)")
+	rootCmd.Flags().Float64VarP(&playSpeed, "speed", "s", 1, "The speed of the audio") 
 }
-
 
